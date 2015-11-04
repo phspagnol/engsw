2bfbd8b6e3e8240b35a80f67a2d0835ac365f69b
PHInfoNote <PHInfoNote@PHInfoNote-PC>
2015-11-04 10:11:19 -0200

    ComitTolla

---
+++ EngSwExemploGit/src/Main2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author PHInfoNote
+ */
+public class Main2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String mensagem="Alo Mundo ";
+        String usuario="Paulo";
+    
+    System.out.println(mensagem+" "+usuario);
+    }
+    
+    
+}
